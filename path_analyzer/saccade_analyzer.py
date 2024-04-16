7409c38c78e91ec089c79f98add3505dd5807b66
Adam Roegiest <adam@zuva.ai>
2024-04-16 21:44:00 +0200

    Initial saccade tests

---
+++ path_analyzer/saccade_analyzer.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import numpy as np
+from nltk.util import ngrams,skipgrams
+from collections import Counter, defaultdict
+
+
+class FixationEvent(object):
+    def __init__(self,aoi, x,y):
+        self.aoi = aoi
+        self.x = x
+        self.y = y
+        self.point = np.array([x,y])
+
+class SaccadeEvent(object):
+    def __init__(self,src,dst):
+        self.event = src.aoi+"-"+dst.aoi
+        self.dist = np.linalg.norm(src.point-dst.point)
+        self.src_aoi = src.aoi
+        self.dst_aoi = dst.aoi
+
+class SaccadeAnalyzer(object):
+    def __init__(self,xlsx_file=None,tsv_file=None):
+        if xlsx_file == None and tsv_file == None:
+            raise Exception("required file not present")
+        
+        if xlsx_file != None:
+            self._data = pd.read_excel(xlsx_file)
+        elif tsv_file != None:
+            self._data = pd.read_csv(tsv_file,sep='\t')
+        self._session_paths =self._process_tracking(self._data)
+        self._saccade_paths = self._create_saccade_paths()
+
+    def _determineAOI(self,fixations):
+        for i,elt in enumerate(fixations):
+            if elt == 1:
+                return self.AOIs[i]
+        return "NA"
+    def _process_tracking(self,df):
+        self.AOIs = list(df.columns[12:])
+        session_paths = []
+        fixation = 0
+        session_path = []
+        for idx, row in df.iterrows():
+            newFixation = row['FixationIndex']
+            if newFixation < fixation:
+                session_paths.append(session_path)
+                aoi = self._determineAOI(row[12:])
+                session_path = [FixationEvent(aoi,row['FixationPointX (MCSpx)'],row['FixationPointX (MCSpx)'])]
+            else:
+                aoi = self._determineAOI(row[12:])
+                session_path.append(FixationEvent(aoi,row['FixationPointX (MCSpx)'],row['FixationPointX (MCSpx)']))
+            fixation = newFixation
+        session_paths.append(session_path)
+        return session_paths
+    
+    def _create_saccade_paths(self):
+        saccade_paths = []
+        for session in self._session_paths:
+            if len(session) <= 2:
+                saccade_paths.append([])
+                continue
+            saccade_path = []
+            old_fixation = session[0]
+            for idx in range(1,len(session)):
+                new_fixation = session[idx]
+                saccade_path.append(SaccadeEvent(old_fixation,new_fixation))
+                old_fixation = new_fixation
+            saccade_paths.append(saccade_path)
+
+        return saccade_paths
+        
+    def print_paths(self):
+        for idx, session in enumerate(self._session_paths):
+            print(f"Session {idx}")
+            for idx,fixation in enumerate(session):
+                print("{0} => {1}".format(''.join([" " for i in range(idx)]),fixation.aoi))
+            # print("Session {0}: {1}".format(idx,' => '.join("{0}".format(fixation.aoi) for fixation in session)))
+            break
+    def print_saccade_paths(self):
+        for idx, session in enumerate(self._saccade_paths):
+            print(f"Session {idx}")
+            for idx,saccade in enumerate(session):
+                print("{0} => {1} ({2})".format(''.join([" " for i in range(idx)]),saccade.event,saccade.dist))
+            # print("Session {0}: {1}".format(idx,' => '.join("{0} ({1})".format(saccade.event,saccade.dist) for saccade in session)))
+            break
+
+    def saccade_path_stats(self):
+        saccade_data = []
+        for idx, session in enumerate(self._saccade_paths):
+            distances = []
+            paired = defaultdict(list)
+            for saccade in session:
+                distances.append(saccade.dist)
+                paired[saccade.event].append(saccade.dist)
+            saccade_data.append(tuple([idx,np.sum(distances),np.mean(distances),np.std(distances),paired]))
+        return saccade_data
+    
+    def saccade_distance_to(self,aoi,ignore_self=False,ignore_nonAoI = False):
+        saccade_data = []
+        for idx, session in enumerate(self._saccade_paths):
+            dists = []
+            for saccade in session:
+                if saccade.dst_aoi == aoi:
+                    if ignore_nonAoI and saccade.src_aoi == "NA":
+                        continue
+                    if ignore_self and saccade.src_aoi == aoi:
+                        continue
+                    dists.append(saccade.dist)
+            saccade_data.append(dists)
+        return saccade_data
+
+            
--- run_analyzer.py
+++ run_analyzer.py
@@ -1,27 +1,44 @@
-from path_analyzer import PathAnalyzer
+from saccade_analyzer import SaccadeAnalyzer
 import numpy as np
 
-spa = PathAnalyzer(tsv_file='test.tsv')
-print("Scan paths:")
-spa.print_paths()
+# spa = PathAnalyzer(tsv_file='test.tsv')
+# print("Scan paths:")
+# spa.print_paths()
 
-print("Query for specific path AOI_2 followed by AOI_3:")
-print("Exact paths from AOI_2 to AOI_3: ",spa.approximate_sequence(['AOI_2','AOI_3']),"\n")
-print("1 gap paths from AOI_2 to AOI_3: ",spa.approximate_sequence(['AOI_2','AOI_3'],1),"\n")
-print("2 gap paths from AOI_2 to AOI_3: ",spa.approximate_sequence(['AOI_2','AOI_3'],2),"\n")
-print("Most common subsequences of max length 3 with a max gap of 2: ")
-mostCommon,sessionPaths = spa.common_subsequences(max_ngram=3,max_gap=2)
-for i in mostCommon:
-    print("Count: {0}; Path {1}".format(i[1],i[0]))
-print("\n\n")
-for sessid, commonPaths in sessionPaths.items():
-    print("Session {0} Common subpaths: {1}".format(sessid,commonPaths))
+# print("Query for specific path AOI_2 followed by AOI_3:")
+# print("Exact paths from AOI_2 to AOI_3: ",spa.approximate_sequence(['AOI_2','AOI_3']),"\n")
+# print("1 gap paths from AOI_2 to AOI_3: ",spa.approximate_sequence(['AOI_2','AOI_3'],1),"\n")
+# print("2 gap paths from AOI_2 to AOI_3: ",spa.approximate_sequence(['AOI_2','AOI_3'],2),"\n")
+# print("Most common subsequences of max length 3 with a max gap of 2: ")
+# mostCommon,sessionPaths = spa.common_subsequences(max_ngram=3,max_gap=2)
+# for i in mostCommon:
+#     print("Count: {0}; Path {1}".format(i[1],i[0]))
+# print("\n\n")
+# for sessid, commonPaths in sessionPaths.items():
+#     print("Session {0} Common subpaths: {1}".format(sessid,commonPaths))
 
-print("\n\n")
-revisits = spa.aoi_revisits()
-print("=========Session revisits=========")
-for sessid, all_revisits in revisits.items():
-    for aoi, revisits in all_revisits.items():
-        meanTime = np.nan_to_num(np.mean([ visit[2] for visit in revisits[1]]))
-        meanLength = np.nan_to_num(np.mean([len(visit[3]) for visit in revisits[1]]))
-        print("Session: {0}, AOI: {1} revisit count: {2}; mean revisit latency: {3}; mean revisit path length: {4} ".format(sessid,aoi,revisits[0],meanTime,meanLength))
+# print("\n\n")
+# revisits = spa.aoi_revisits()
+# print("=========Session revisits=========")
+# for sessid, all_revisits in revisits.items():
+#     for aoi, revisits in all_revisits.items():
+#         meanTime = np.nan_to_num(np.mean([ visit[2] for visit in revisits[1]]))
+#         meanLength = np.nan_to_num(np.mean([len(visit[3]) for visit in revisits[1]]))
+#         print("Session: {0}, AOI: {1} revisit count: {2}; mean revisit latency: {3}; mean revisit path length: {4} ".format(sessid,aoi,revisits[0],meanTime,meanLength))
+
+sa = SaccadeAnalyzer(xlsx_file="test.xlsx")
+print(sa.AOIs)
+sa.print_paths()
+sa.print_saccade_paths()
+data = sa.saccade_path_stats()
+d = data[0]
+print(d[0],d[1],d[2],d[3])
+for k,v in d[4].items():
+    print("{0} : {1} times:{2:.2f} ({3:.2f})".format(k,len(v),np.mean(v),np.std(v)))
+
+data = sa.saccade_distance_to("AOI[Cover photos 2]Hit")
+print(data[0])
+data = sa.saccade_distance_to("AOI[Cover photos 2]Hit",ignore_self=True)
+print(data[0])
+data = sa.saccade_distance_to("AOI[Cover photos 2]Hit",ignore_self=True,ignore_nonAoI=True)
+print(data[0])
